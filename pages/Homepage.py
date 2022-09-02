d7d9d4ab1527f5593b9be2a5266c2a0712f37914
Alistair Walsh <alistair.walsh@me.com>
2022-09-02 11:21:26 +1000

    test requests

--- pages/Homepage.py
+++ pages/Homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import requests as r
+import requests
 from io import StringIO
 
 st.title("Homepage")
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import requests as r
+import requests
 from io import StringIO
 
 # https://education.arcus.chop.edu/redcap-api/
@@ -11,3 +11,15 @@ st.title("hello")
 
 # df = pd.read_csv(StringIO(r.text))
 # df
+
+data = {
+    'token': 'C0ADB9762F115D7200E32784AAE3E3D0',
+    'content': 'project',
+    'format': 'json',
+    'returnFormat': 'json'
+}
+#r = requests.post('https://redcap.latrobe.edu.au/redcap/api/',data=data)
+#print('HTTP Status: ' + str(r.status_code))
+#print(r.json())
+
+st.write(data)
