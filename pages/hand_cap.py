4f56cf0695d17ad9e3513e8a9b315997ae0ab4c3
Alistair Walsh <alistair.walsh@me.com>
2022-09-06 11:40:41 +1000

    playing with cv2 for media pipe

--- pages/hand_cap.py
+++ pages/hand_cap.py
@@ -1,5 +1,5 @@
 from streamlit_webrtc import webrtc_streamer
-#import cv2
+import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
