5526028e3f2aa5c67192a768b9c05833603d1c43
Alistair Walsh <alistair.walsh@me.com>
2022-09-06 11:49:38 +1000

    removed some mediapipe imports

--- pages/hand_cap.py
+++ pages/hand_cap.py
@@ -1,8 +1,8 @@
 from streamlit_webrtc import webrtc_streamer
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+# mp_drawing = mp.solutions.drawing_utils
+# mp_drawing_styles = mp.solutions.drawing_styles
+# mp_hands = mp.solutions.hands
 
 # For static images:
 # IMAGE_FILES = []
