3550d4bb46b0c3d20407fd60efbbb6b92cc494fc
Alistair Walsh <alistair.walsh@me.com>
2022-09-06 11:13:27 +1000

    added iceserver

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,12 @@ from io import StringIO
 
 st.title("hello")
 
-webrtc_streamer(key="sample")
+webrtc_streamer(
+    key="sample",
+    rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+    )
 
 # df = pd.read_csv(StringIO(r.text))
 # df
