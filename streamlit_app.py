499b494ba968dbef879c0581663d758ca6111c99
Alistair Walsh <alistair.walsh@me.com>
2022-09-06 11:35:36 +1000

    added process loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,14 @@ from io import StringIO
 
 st.title("hello")
 
+def video_frame_callback(frame):
+    image = frame.to_ndarray(format="bgr24")
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
 webrtc_streamer(
-    key="sample",
+    key="example",
+    video_frame_callback=video_frame_callback,
     rtc_configuration={  # Add this config
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
