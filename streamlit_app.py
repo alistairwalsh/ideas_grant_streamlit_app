8d6379eec92d8e6fb283c40e41f1eef8efbe9092
Alistair Walsh <alistair.walsh@me.com>
2022-09-06 10:39:57 +1000

    add webrtc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 import pandas as pd
 import numpy as np
 import requests
@@ -9,21 +10,23 @@ from io import StringIO
 
 st.title("hello")
 
+webrtc_streamer(key="example")
+
 # df = pd.read_csv(StringIO(r.text))
 # df
 
-picture = st.camera_input("Take a picture")
+#picture = st.camera_input("Take a picture")
 
-if picture:
-     st.image(picture)
+#if picture:
+#     st.image(picture)
 
-data = {
-    'token': 'C0ADB9762F115D7200E32784AAE3E3D0',
-    'content': 'metadata',
-    'format': 'json',
-    'returnFormat': 'json'
-}
-r = requests.post('https://redcap.latrobe.edu.au/redcap/api/',data=data)
-st.write('HTTP Status: ' + str(r.status_code))
-st.write(r.json())
-st.write(data)
+#data = {
+#    'token': 'C0ADB9762F115D7200E32784AAE3E3D0',
+#    'content': 'metadata',
+#    'format': 'json',
+#    'returnFormat': 'json'
+#}
+#r = requests.post('https://redcap.latrobe.edu.au/redcap/api/',data=data)
+#st.write('HTTP Status: ' + str(r.status_code))
+#st.write(r.json())
+#st.write(data)
